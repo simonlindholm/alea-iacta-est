f36a8bd833d2f6198c56044afbdad3a98375261a
Johan Sannemo <johan.sannemo@gmail.com>
2015-04-11 13:31:48 +0200

    Add client

--- sublight/game.js
+++ sublight/game.js
@@ -1,3 +1,46 @@
+var socket = io.connect("http://"+document.domain+":9502")
+var ctx = canvas.getContext("2d");
+var width = 800;
+var height = 600;
 
-var canvas = window.canvas;
+socket.on("connect", function(){
+    socket.emit("init", {});
+});
 
+var defender = false;
+
+socket.on("waiting", function(){
+    defender = true;
+    drawStatus("Waiting for opponent...");
+    drawBackground();
+});
+
+socket.on("round", function(){
+    showControls(defender);
+});
+
+
+function DefenderInput(socket) {
+    this.socket = socket;
+
+}
+DefenderInput.prototype.createListeners = function(){
+    var input = this;
+    $("canvas").keydown(function(e){
+        console.log(e.which);
+    });
+}
+
+function drawBackground(){
+    ctx.stroke = "#000000";
+    ctx.strokeRect(0, 0, width, height);
+}
+
+function drawStatus(str){
+    ctx.fillText(str, width/2 - 50, height/2);
+}
+
+function showControls(isDefender){
+    console.log("#defeinder-"+isDefender);
+    $("#defender-"+isDefender).show();
+}
